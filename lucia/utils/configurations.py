b29a617f629e74493431c08e8b605bcf2ebb658c
Carter Temm <crtbraille@gmail.com>
2018-07-28 16:31:00 -0500

    fixed configuration indentation

--- lucia/utils/configurations.py
+++ lucia/utils/configurations.py
@@ -48,11 +48,11 @@ class Configuration:
 	def write_boolean(self, section, key, value):
 		self.config[section][key]=bool(value)
 
-def load_file(self, filename):
+	def load_file(self, filename):
 		with open(filename, "r") as f:
 			self.read_string(f.read(), "<string>")
 
-def save_file(self, filename):
+	def save_file(self, filename):
 		with open(filename, "w") as f:
 			self.config.write(f)
 
