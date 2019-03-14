878c8ad19732811398f4285f1fb7c45526959eaa
kianoosh-shakeri <kianooshshakeri7@gmail.com>
2019-03-15 00:34:54 +0330

    added a needed class to the module

--- lucia/ui/menu2.py
+++ lucia/ui/menu2.py
@@ -30,6 +30,26 @@ class SpinboxDialog(wx.Dialog):
     def GetValue(self):
         return self.input.GetValue()
 
+class TextEntryDialog(wx.Dialog):
+    def __init__(self, parent, title, caption):
+        style = wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
+        super(TextEntryDialog, self).__init__(parent, -1, title, style=style)
+        text = wx.StaticText(self, -1, caption)
+        input = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE)
+        input.SetInitialSize((400, 300))
+        buttons = self.CreateButtonSizer(wx.OK|wx.CANCEL)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(text, 0, wx.ALL, 5)
+        sizer.Add(buttons, 0, wx.EXPAND|wx.ALL, 5)
+        sizer.Add(input, 1, wx.EXPAND|wx.ALL, 5)
+        input.SetFocus()
+        self.SetSizerAndFit(sizer)
+        self.input = input
+    def SetValue(self, value):
+        self.input.SetValue(value)
+    def GetValue(self):
+        return self.input.GetValue()
+
 def getinput(title, message, multiline=False, value="", mode="plaintext"):
 	text=""
 	if mode=="plaintext":
