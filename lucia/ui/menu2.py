b20b49b5fea308b05f47ace6ff6772a2d05fea87
kianoosh-shakeri <kianooshshakeri7@gmail.com>
2019-03-17 18:10:38 +0330

    removed support for wx in menu2 and instead added support for lucia.ui.virtualinput

--- lucia/ui/menu2.py
+++ lucia/ui/menu2.py
@@ -1,9 +1,7 @@
 """enhanced menu for advance uses. Make sure you create wx.App and call the menu before creating any menus with this module. See examples/demmo1/main2.py if you need an idea how this module is used"""
-import wx
 import lucia
 from lucia.audio.soundpool import *
-import logging
-logging.basicConfig(filename="menu tracebacks.log", level=logging.DEBUG)
+from lucia.ui.virtualinput import *
 
 #Events
 CANCELEVENT=0
@@ -11,62 +9,17 @@ CANCELEVENT=0
 import sys
 import time
 from lucia import output
-class SpinboxDialog(wx.Dialog):
-    def __init__(self, parent, title, caption, value=0):
-        style = wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
-        super(SpinboxDialog, self).__init__(parent, -1, title, style=style)
-        text = wx.StaticText(self, -1, caption)
-        input = wx.SpinCtrl(self, -1, value=str(value), max=2000000000, size=(400,300))
-        buttons = self.CreateButtonSizer(wx.OK|wx.CANCEL)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(text, 0, wx.ALL, 5)
-        sizer.Add(buttons, 0, wx.EXPAND|wx.ALL, 5)
-        sizer.Add(input, 1, wx.EXPAND|wx.ALL, 5)
-        input.SetFocus()
-        self.SetSizerAndFit(sizer)
-        self.input = input
-    def SetValue(self, value):
-        self.input.SetValue(value)
-    def GetValue(self):
-        return self.input.GetValue()
-
-class TextEntryDialog(wx.Dialog):
-    def __init__(self, parent, title, caption):
-        style = wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
-        super(TextEntryDialog, self).__init__(parent, -1, title, style=style)
-        text = wx.StaticText(self, -1, caption)
-        input = wx.TextCtrl(self, -1, style=wx.TE_MULTILINE)
-        input.SetInitialSize((400, 300))
-        buttons = self.CreateButtonSizer(wx.OK|wx.CANCEL)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(text, 0, wx.ALL, 5)
-        sizer.Add(buttons, 0, wx.EXPAND|wx.ALL, 5)
-        sizer.Add(input, 1, wx.EXPAND|wx.ALL, 5)
-        input.SetFocus()
-        self.SetSizerAndFit(sizer)
-        self.input = input
-    def SetValue(self, value):
-        self.input.SetValue(value)
-    def GetValue(self):
-        return self.input.GetValue()
 
 def getinput(title, message, multiline=False, value="", mode="plaintext"):
-	text=""
 	if mode=="plaintext":
 		if multiline:
-			input=TextEntryDialog(None, message, title)
+			input=VirtualInput("enter value", value=value)
 		else:
-			input=wx.TextEntryDialog(None, message, title)
-		input.SetValue(value)
-		input.ShowModal()
-		text=input.GetValue()
-		input.Destroy()
+			input=VirtualInput("enter value", value=value)
+			#Later this will have multilinebox enabled so this condition remains still
 	elif mode=="spinbox":
-		input=SpinboxDialog(None, message, title)
-		input.SetValue(value)
-		input.ShowModal()
-		text=input.GetValue()
-		input.Destroy()
+		input=VirtualInput("enter value", whitelist=WHITELIST_DIGITS, value=value)
+	text=input.run()
 	return text
 
 class menuitem:
