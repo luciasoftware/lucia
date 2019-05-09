c248ab34988552c2f6a0bf056dc33eeaf3eb137f
NicklasMCHD <NicklasMCHD@live.dk>
2019-05-09 09:02:25 +0200

    Started work on bass soundpool

--- lucia/audio/bass/soundpool.py
+++ lucia/audio/bass/soundpool.py
@@ -1,4 +1,23 @@
 import lucia
+from sound_lib import listener, main, music, input, output, stream
 
 class SoundPool(lucia.audio.SoundPool):
-	pass
+	def __init__(self, *args):
+		self.output = output.ThreeDOutput(args)
+		self.output.start()
+		self.listener = listener.Listener()
+		self.sources = []
+
+	def play_stationary(self, soundfile):
+		source = None
+		if soundfile == "":
+			raise ValueError("No audio data provided")
+		if isinstance(soundfile, str):
+			source = stream.FileStream(mem=False, soundfile)
+		if isinstance(soundfile, str):
+			source = stream.FileStream(mem=False, soundfile)
+		else:
+			source = stream.FileStream(mem=True, io.BytesIO(soundfile)
+		source.play()
+		self.sources.append(source)
+		return source
