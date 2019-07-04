62d14bbb0df303d61a579f568fdd795b6376364c
NicklasMCHD <NicklasMCHD@live.dk>
2019-07-04 22:48:53 +0200

    Added test cases for lucia/data.py

---
+++ tests/test_data.py
@@ -0,0 +1,23 @@
+"""tests lucias data module"""
+from lucia import data
+
+def test_compression():
+	original = b"This is a string, that will be changed in many ways."
+	
+	compressed = data.compress(original)
+	assert compressed is not None
+	assert compressed is not original
+	decompressed = data.decompress(compressed)
+	assert original == decompressed
+
+def test_encryption():
+	original = "This is a plain string, that will be encrypted and then decrypted - let's see how it goes."
+	key = "ThisIsMyVerySecretKey123456789"
+	
+	encrypted = data.encrypt(original, key)
+	decrypted = data.decrypt(encrypted, key)
+	
+	assert original == decrypted
+
+test_compression()
+test_encryption()
