f58f1451cc70a807dee12d5dcf5a3a02b17bf5de
Carter Temm <crtbraille@gmail.com>
2019-07-06 11:22:20 -0500

    Add pause and resume functions to timer

--- lucia/utils/timer.py
+++ lucia/utils/timer.py
@@ -21,12 +21,16 @@ class Timer:
 
 	def __init__(self):
 		self.inittime = time.time()
+		self.paused = 0
 
 	@property
 	def elapsed(self):
 		"""Returns the exact elapsed time since this timer was created or last restarted.
 		"""
-		return self._ms(time.time() - self.inittime)
+		if self.paused != 0:
+			return self.paused
+		else:
+			return self._ms(time.time() - self.inittime)
 
 	@elapsed.setter
 	def elapsed(self, amount):
@@ -35,12 +39,24 @@ class Timer:
 		Args:
 			amount (int): The time elapsed (in millis)
 		"""
-		self.inittime = time.time() - (amount / 1000)
+		if self.paused == 0:
+			self.inittime = time.time() - (amount / 1000)
+		else:
+			self.paused = amount
 
 	def restart(self):
 		"""Restarts the timer, and set it's elapsed time to 0.
 		"""
 		self.__init__()
+
+	def pause(self):
+		"""Pauses the timer at a certain position."""
+		self.paused = self._ms(time.time() - self.inittime)
+
+	def resume(self):
+		"""Resumes the timer after being paused."""
+		self.inittime = time.time() - (self.paused / 1000)
+		self.paused = 0
 
 	def _ms(self, t):
 		return int(round(t*1000))
