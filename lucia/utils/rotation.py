0b96a9b0fe4464647acbec9e8b76d3cf2560fe27
kianoosh-shakeri <kianooshshakeri7@gmail.com>
2019-10-15 22:50:39 +0330

    fixed #1

--- lucia/utils/rotation.py
+++ lucia/utils/rotation.py
@@ -3,20 +3,20 @@
 # modified by LuciaSoftware and it's contributors.
 """Rotation class
 
-This is modeled on the bgt rotation class from Sam Tupy, however it has been converted to use trigonometric functions and degrees properly. Input and output is still in degrees, however east is 0, north is 90, south is 270, etc.
+This is modeled on the bgt rotation class from Sam Tupy. Input and output are in degrees.
 """
 import lucia
 from math import pi, sin, cos, atan, radians, degrees, sqrt
 
 
-_east = 0
+_east = 90
 _northeast = 45
-_north = 90
-_northwest = 135
-_west = 180
+_north = 0
+_northwest = 315
+_west = 270
 _southwest = 225
-_south = 270
-_southeast = 315
+_south = 180
+_southeast = 135
 
 
 class Vector:
@@ -67,8 +67,8 @@ def move(coords, deg, zdeg=0.0,factor=1.0):
 	if deg >= 360:
 		deg = deg - 360
 	r = Vector()
-	r.x = x + factor*cos(radians(deg))
-	r.y = y + factor*sin(radians(deg))
+	r.x = x + factor*sin(radians(deg))
+	r.y = y + factor*cos(radians(deg))
 	r.z = z + factor*sin(radians(zdeg))
 	return r
 
@@ -94,30 +94,30 @@ def getdir(facing):
 
 
 def snapleft(direction, inc=45):
-	d = direction + inc
+	d = direction - inc
 	if d >= 360:
 		d -= 360
 	return d
 
 
 def snapright(direction, inc=45):
-	d = direction - inc
+	d = direction + inc
 	if d < 0:
 		d += 360
 	return d
 
 
 def turnleft(deg, inc=5):
-	deg += inc
-	if deg >= 360:
-		deg -= 360
+	deg -= inc
+	if deg < 0:
+		deg += 360
 	return deg
 
 
 def turnright(deg, inc=5):
-	deg -= inc
-	if deg < 0:
-		deg += 360
+	deg += inc
+	if deg >= 360:
+		deg -= 360
 	return deg
 
 
@@ -198,14 +198,15 @@ def calculate_x_y_angle(x1, y1, x2, y2, deg):
 	y = y2 - y1
 	#handle the case where division by 0 may occur and manually return the angle
 	if x==0:
-		if y>0: return 90
-		if y<0: return 270
+		if y >= 0: return 0
+		if y < 0: return 180
 	rad = atan(y / x)
 	arc_tan = degrees(rad)
 	fdeg = 0
 	if x < 0:
-		fdeg = 180 + arc_tan
-	else: fdeg=arc_tan
+		fdeg = 270 - arc_tan
+	elif x > 0:
+		fdeg = 270 - arc_tan
 	fdeg-=deg
 	if fdeg < 0:
 		fdeg += 360
@@ -213,55 +214,3 @@ def calculate_x_y_angle(x1, y1, x2, y2, deg):
 		fdeg-=360
 	return fdeg
 
-def calculate_x_y_string(deg):
-	if deg == 0:
-		return "streight off to the right"
-	elif deg > 0 and deg < 10:
-		return "off to the right and very slightly in front"
-	elif deg > 9 and deg < 30:
-		return "mostly off to the right and slightly in front"
-	elif deg > 30 and deg < 60:
-		return "in front and off to the right"
-	elif deg > 59 and deg < 80:
-		return "in front and a bit off to the right"
-	elif deg > 79 and deg < 85:
-		return "in front and a tiny bit off to the right"
-	elif deg > 84 and deg < 90:
-		return "in front and very slightly to the right"
-	elif deg == 90:
-		return "straight in front"
-	elif deg > 90 and deg < 96:
-		return "in front and very slightly to the left"
-	elif deg > 965 and deg < 101:
-		return "in front and a tiny bit off to the left"
-	elif deg > 100 and deg < 120:
-		return "in front and a bit off to the left"
-	elif deg > 120 and deg < 150:
-		return "in front and off to the left"
-	elif deg > 149 and deg < 170:
-		return "mostly off to the left and slightly in front"
-	elif deg > 169 and deg < 180:
-		return "off to the left and very slightly in front"
-	elif deg == 180:
-		return "straight off to the left"
-	elif deg>180 and deg<200:
-		return "off to the left and very slightly behind"
-	elif deg>199 and deg<220:
-		return "mostly off to the left and slightly behind"
-	elif deg > 219 and deg < 240:
-		return "behind and off to the left"
-	elif deg > 239 and deg < 270:
-		return "slightly behind and far off to the left"
-	elif deg == 270:
-		return "streight behind"
-	elif deg > 270 and deg < 300:
-		return "far off to the left"
-	elif deg > 299 and deg < 320:
-		return "a ways off to the left"
-	elif deg > 319 and deg < 340:
-		return "a little ways off to the left"
-	elif deg > 339 and deg < 350:
-		return "slightly off to the left"
-	elif deg > 349 and deg < 360:
-		return "verry slightly off to the left"
-	return ""
