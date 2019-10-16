9bef7791256cfa768f827c0cc70d7d2afcbafc3e
kianoosh-shakeri <kianooshshakeri7@gmail.com>
2019-10-16 21:11:22 +0330

    fixed the rotation module. Started to work on compatibility with openal

--- lucia/utils/rotation.py
+++ lucia/utils/rotation.py
@@ -8,7 +8,7 @@ This is modeled on the bgt rotation class from Sam Tupy. Input and output are in
 import lucia
 from math import pi, sin, cos, atan, radians, degrees, sqrt
 
-
+# Directions.
 _east = 90
 _northeast = 45
 _north = 0
@@ -18,6 +18,9 @@ _southwest = 225
 _south = 180
 _southeast = 135
 
+# Coordinate systems
+left_handed=0
+right_handed=1
 
 class Vector:
 	"""an object representing what bgt calls a vector
@@ -43,34 +46,134 @@ class Vector:
 
 	coords = property(get_coords, set_coords)
 
+
 	@property
 	def get_tuple(self):
 		return tuple([round(self.x), round(self.y), round(self.z)])
 
+class LeftHanded:
+	def move(coords, deg, pitch=0.0,factor=1.0):
+		"""moves a vector in a given direction by a scale factor of factor
+		
+		Takes a vector as input, applies the translation, and returns a vector as output. Probably best done as player.coords=move(player.coords,player.facing) or something similar.
+		The scale factor is used if you wish to move more than 1 coordinate, otherwise you simply apply the unit circle.
+		
+		args:
+				coords (tuple or list): The current point in 3-space you wish to move.
+				deg (float): The direction you wish to move. In this degree system east is 0, north is 90, west is 180 and south is 270.
+				pitch (float, optional): The vertical degrees you wish to move. Defaults to 0, no vertical movement.
+				factor (float, optional): The scale factor you wish to move by. Passing 1 is equivalent to one unit move in any direction, but for warping in a particular direction you can pass a higher factor. Defaults to 1.
+		
+		returns:
+				a transformed vector
+		"""
+		x, y, z = coords
+		if deg >= 360:
+			deg = deg - 360
+		r = Vector()
+		r.x = x + factor*sin(radians(deg))
+		r.y = y + factor*cos(radians(deg))
+		r.z = z + factor*sin(radians(pitch))
+		return r
+	def calculate_angle(x1, y1, x2, y2, deg):
+		"""given two points, returns the angle of the second one relative to the first.
+		
+		This function is useful for reporting a direction of an object to a player, for example.
+		In this example the 'origin' point would be the player and the 'distant' point would be the object the player is tracking.
+		
+		args:
+				x1 (float): The x coordinate of the origin point.
+				y1 (float): The y coordinate of the origin point.
+				x2 (float): The x coordinate of the distant point
+				y2 (float): The y coordinate of the distant point
+				deg (float): The absolute direction the origin point is facing, for offsets.
+		
+		returns:
+				an angle (in degrees) of the distant point relative to the origin point, shifted by the orientation of the origin.
+		"""
+		x = x2 - x1
+		y = y2 - y1
+		#handle the case where division by 0 may occur and manually return the angle
+		if x==0:
+			if y >= 0: return 0
+			if y < 0: return 180
+		rad = atan(y / x)
+		arc_tan = degrees(rad)
+		fdeg = 0
+		if x < 0:
+			fdeg = 270 - arc_tan
+		elif x > 0:
+			fdeg = 270 - arc_tan
+		fdeg-=deg
+		if fdeg < 0:
+			fdeg += 360
+		elif fdeg>360:
+			fdeg-=360
+		return fdeg
 
-def move(coords, deg, zdeg=0.0,factor=1.0):
-	"""moves a vector in a given direction by a scale factor of factor
-	
-	Takes a vector as input, applies the translation, and returns a vector as output. Probably best done as player.coords=move(player.coords,player.facing) or something similar.
-	The scale factor is used if you wish to move more than 1 coordinate, otherwise you simply apply the unit circle.
-	
-	args:
-	    coords (Vector): The current point in 3-space you wish to move.
-	    deg (float): The direction you wish to move. In this degree system east is 0, north is 90, west is 180 and south is 270.
-	    zdeg (float, optional): The vertical degrees you wish to move. Defaults to 0, no vertical movement.
-	    factor (float, optional): The scale factor you wish to move by. Passing 1 is equivalent to one unit move in any direction, but for warping in a particular direction you can pass a higher factor. Defaults to 1.
-	
-	returns:
-	    a transformed vector
-	"""
-	x, y, z = coords
-	if deg >= 360:
-		deg = deg - 360
-	r = Vector()
-	r.x = x + factor*sin(radians(deg))
-	r.y = y + factor*cos(radians(deg))
-	r.z = z + factor*sin(radians(zdeg))
-	return r
+class RightHanded:
+	def move(coords, deg, pitch=0.0,factor=1.0):
+		"""moves a vector in a given direction by a scale factor of factor
+		
+		Takes a vector as input, applies the translation, and returns a vector as output. Probably best done as player.coords=move(player.coords,player.facing) or something similar.
+		The scale factor is used if you wish to move more than 1 coordinate, otherwise you simply apply the unit circle.
+		
+		args:
+				coords (tuple or list): The current point in 3-space you wish to move.
+				deg (float): The direction you wish to move. In this degree system east is 0, north is 90, west is 180 and south is 270.
+				pitch (float, optional): The vertical degrees you wish to move. Defaults to 0, no vertical movement.
+				factor (float, optional): The scale factor you wish to move by. Passing 1 is equivalent to one unit move in any direction, but for warping in a particular direction you can pass a higher factor. Defaults to 1.
+		
+		returns:
+				a transformed vector
+		"""
+		x, y, z = coords
+		if deg >= 360:
+			deg = deg - 360
+		r = Vector()
+		r.x = x + factor*sin(radians(deg))
+		r.y = y + factor*sin(radians(pitch))
+		r.z = z + factor*cos(radians(deg))
+		if z < 0:
+			r.z=abs(r.z)
+		else:
+			r.z=-r.z
+		return r
+	def calculate_angle(x1, z1, x2, z2, deg):
+		"""given two points, returns the angle of the second one relative to the first.
+		
+		This function is useful for reporting a direction of an object to a player, for example.
+		In this example the 'origin' point would be the player and the 'distant' point would be the object the player is tracking.
+		
+		args:
+				x1 (float): The x coordinate of the origin point.
+				z1 (float): The z coordinate of the origin point.
+				x2 (float): The x coordinate of the distant point
+				z2 (float): The y coordinate of the distant point
+				deg (float): The absolute direction the origin point is facing, for offsets.
+		
+		returns:
+				an angle (in degrees) of the distant point relative to the origin point, shifted by the orientation of the origin.
+		"""
+		x = x2 - x1
+		z = z1 - z2
+		#handle the case where division by 0 may occur and manually return the angle
+		if x==0:
+			if z > 0: return 180
+			if z <= 0: return 0
+		rad = atan(z / x)
+		arc_tan = degrees(rad)
+		fdeg = 0
+		if x < 0:
+			fdeg = 270 - arc_tan
+		elif x > 0:
+			fdeg = 270 - arc_tan
+		fdeg-=deg
+		if fdeg < 0:
+			fdeg += 360
+		elif fdeg>360:
+			fdeg-=360
+		return fdeg
 
 
 def getdir(facing):
@@ -121,43 +224,6 @@ def turnright(deg, inc=5):
 	return deg
 
 
-def dir_to_string(facing):
-	r = str()
-	if facing > 0 and facing < 45:
-		r = "east north east"
-	if facing > 45 and facing < 90:
-		r = "north north east"
-	if facing > 90 and facing < 135:
-		r = "north north west"
-	if facing > 135 and facing < 180:
-		r = "west north west"
-	if facing > 180 and facing < 225:
-		r = "west south west"
-	if facing > 225 and facing < 270:
-		r = "south south west"
-	if facing > 270 and facing < 315:
-		r = "south south east"
-	if facing > 315:
-		r="east southeast"
-	if facing == 0:
-		r = "east"
-	if facing == 45:
-		r = "northeast"
-	if facing == 90:
-		r = "north"
-	if facing == 135:
-		r = "northwest"
-	if facing == 180:
-		r = "west"
-	if facing == 225:
-		r = "southwest"
-	if facing == 270:
-		r = "south"
-	if facing == 315:
-		r = "southeast"
-	return r
-
-
 def get_1d_distance(x1, x2):
 	"""returns the distance on a 1-dimensional plane from x1 to x2"""
 	return abs(x1 - x2)
@@ -176,41 +242,3 @@ def get_3d_distance(x1, y1, z1, x2, y2, z2):
 	y = get_1d_distance(y1, y2)
 	z = get_1d_distance(z1, z2)
 	return sqrt(x * x + y * y + z * z)
-
-
-def calculate_x_y_angle(x1, y1, x2, y2, deg):
-	"""given two points, returns the angle of the second one relative to the first.
-	
-	This function is useful for reporting a direction of an object to a player, for example.
-	In this example the 'origin' point would be the player and the 'distant' point would be the object the player is tracking.
-	
-	args:
-	    x1 (float): The x coordinate of the origin point.
-	    y1 (float): The y coordinate of the origin point.
-	    x2 (float): The x coordinate of the distant point
-	    y2 (float): The y coordinate of the distant point
-	    deg (float): The absolute direction the origin point is facing, for offsets.
-	
-	returns:
-	    an angle (in degrees) of the distant point relative to the origin point, shifted by the orientation of the origin.
-	"""
-	x = x2 - x1
-	y = y2 - y1
-	#handle the case where division by 0 may occur and manually return the angle
-	if x==0:
-		if y >= 0: return 0
-		if y < 0: return 180
-	rad = atan(y / x)
-	arc_tan = degrees(rad)
-	fdeg = 0
-	if x < 0:
-		fdeg = 270 - arc_tan
-	elif x > 0:
-		fdeg = 270 - arc_tan
-	fdeg-=deg
-	if fdeg < 0:
-		fdeg += 360
-	elif fdeg>360:
-		fdeg-=360
-	return fdeg
-
