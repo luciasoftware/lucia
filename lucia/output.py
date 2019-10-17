b0fcf95520daf46f4da4c6dd3e4a3a9fca89b94b
NicklasMCHD <NicklasMCHD@live.dk>
2019-10-17 13:51:32 +0200

    Fixed indentation in lucia/output.py

--- lucia/output.py
+++ lucia/output.py
@@ -18,19 +18,19 @@ import os
 # import ao2, check for Runtime Error (missing espeak binaries on ReadTheDocs).
 try:
 	from accessible_output2.outputs.auto import *
+	output = Auto()
+	"""
+	Functions:
+	get_first_available_output() - autoselects the first available output.
+	speak(text, interrupt=False) - speaks some text.
+	silence() - Silence the current output if speaking.
+	braille(text, **options) - braills some output.
+	output(text, *args) - Both speak and braille some text.
+	is_active() - checks if the given output is active.
+	"""
 except RuntimeError:
 	if platform.system() == "Linux" and "readthedocs.org" in os.getcwd():
 		pass
 	else:
 		raise
 
-output = Auto()
-"""
-Functions:
-get_first_available_output() - autoselects the first available output.
-speak(text, interrupt=False) - speaks some text.
-silence() - Silence the current output if speaking.
-braille(text, **options) - braills some output.
-output(text, *args) - Both speak and braille some text.
-is_active() - checks if the given output is active.
-"""
