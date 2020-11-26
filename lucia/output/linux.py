8bbba8d37a28accf78a063db114352a7bb753140
NicklasTegner <NicklasMCHD@live.dk>
2020-11-26 21:36:35 +0100

    Initial work on lucia 2.0 output

---
+++ lucia/output/linux.py
@@ -0,0 +1,5 @@
+# Copyright LuciaSoftware 2019 - 2020
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE or copy at
+# https://www.boost.org/LICENSE_1_0.txt)
