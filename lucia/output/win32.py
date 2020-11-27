79ddb3800acb55cd5b29f4da114aca8c952edd64
NicklasTegner <NicklasMCHD@live.dk>
2020-11-27 16:56:10 +0100

    Fixes to win32 output on non win platforms when coding lucia

--- lucia/output/win32.py
+++ lucia/output/win32.py
@@ -3,7 +3,7 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE or copy at
 # https://www.boost.org/LICENSE_1_0.txt)
-from cytolk import tolk
+from cytolk import tolk # type: ignore
 
 from lucia.output.generic import GenericOutputDriver
 
