dc3759ee4d946c256aa18eaa904fe0ac1e410024
NicklasTegner <NicklasMCHD@live.dk>
2020-11-27 16:56:42 +0100

    Added initial linux output through espeak-ng

--- lucia/output/linux.py
+++ lucia/output/linux.py
@@ -3,3 +3,20 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE or copy at
 # https://www.boost.org/LICENSE_1_0.txt)
+from espeakng import ESpeakNG  # type: ignore
+
+from lucia.output.generic import GenericOutputDriver
+
+
+class LinuxOutputDriver(GenericOutputDriver):
+    def __init__(self):
+        self._speech_driver = ESpeakNG()
+
+    def output(self, message, interrupt=True):
+        return self.speak(message, interrupt)
+
+    def speak(self, message, interrupt=True):
+        if interrupt:
+            self._speech_driver.say(" ")
+        self._speech_driver.say(message)
+        return True
